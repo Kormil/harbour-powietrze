7d4f42d2c8b96d68b4c1c3671eb0c4708c601d06
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-10-25 14:18:50 +0800

    Create harbour-powietrze-zh_CN.ts

---
+++ translations/harbour-powietrze-zh_CN.ts
@@ -0,0 +1,389 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <source>Application for checking the current state of air quality.</source>
+        <translation>用于获取当前空气质量数据的软件。</translation>
+    </message>
+    <message>
+        <source>version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <source>Buy me a beer</source>
+        <translation>给我买瓶啤酒</translation>
+    </message>
+</context>
+<context>
+    <name>AirlyPage</name>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation>已启用</translation>
+    </message>
+    <message>
+        <source>Air quality index</source>
+        <translation>空气质量指数</translation>
+    </message>
+    <message>
+        <source>Api key for Airly</source>
+        <translation空气 API</translation>
+    </message>
+    <message>
+        <source>To get access to Airly api you must generate key. You can get your own api key from
+https://developer.airly.eu</source>
+        <translation>你必须生成密钥以访问Airly API, 你可以从下方网站获取你的 API https://developer.airly.eu<</translation>
+    </message>
+</context>
+<context>
+    <name>FindNearestPage</name>
+    <message>
+        <source>Find more</source>
+        <translation>查询更多</translation>
+    </message>
+    <message>
+        <source>Nearest station</source>
+        <translation>最近站点</translation>
+    </message>
+    <message>
+        <source>Wait for GPS</source>
+        <translation>等待 GPS 连接</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>About Powietrze</source>
+        <translation>关于 Powietrze</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Select station</source>
+        <translation>选择站点</translation>
+    </message>
+    <message>
+        <source>Pull down to select stations</source>
+        <translation>下拉以选择站点</translation>
+    </message>
+    <message>
+        <source>Nearest station</source>
+        <translation>最近站点</translation>
+    </message>
+    <message>
+        <source>Favourite</source>
+        <translation>收藏</translation>
+    </message>
+    <message>
+        <source>Only once</source>
+        <translation>仅一次</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 小时</translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation>8 小时</translation>
+    </message>
+    <message>
+        <source>Permanently</source>
+        <translation>永久</translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation>24 小时</translation>
+    </message>
+    <message>
+        <source>Stop locating</source>
+        <translation>停止获取位置</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>ManageProvidersPage</name>
+    <message>
+        <source>Providers</source>
+        <translation>提供商</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>已禁用</translation>
+    </message>
+</context>
+<context>
+    <name>OpenAQPage</name>
+    <message>
+        <source>Enabled</source>
+        <translation><已启用/translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+    <message>
+        <source>Air quality index</source>
+        <translation>空气质量指数</translation>
+    </message>
+</context>
+<context>
+    <name>PowietrzeConnection</name>
+    <message>
+        <source>OTHER</source>
+        <translation type="obsolete">OTHER</translation>
+    </message>
+</context>
+<context>
+    <name>PowietrzePage</name>
+    <message>
+        <source>Enabled</source>
+        <translation>已启用</translation>
+    </message>
+    <message>
+        <source>Air quality index</source>
+        <translation>空气质量指数</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>正常</translation>
+    </message>
+    <message>
+        <source>Pollution names</source>
+        <translation>污染名称</translation>
+    </message>
+    <message>
+        <source>Short</source>
+        <translation>短期</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>ProviderItem</name>
+    <message>
+        <source>Name </source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <source>Site </source>
+        <translation>网址</translation>
+    </message>
+</context>
+<context>
+    <name>ProviderLicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Poland</source>
+        <translation>波兰</translation>
+    </message>
+    <message>
+        <source>OTHER</source>
+        <translation>其它</translation>
+    </message>
+    <message>
+        <source>Good</source>
+        <translation>佳</translation>
+    </message>
+    <message>
+        <source>Fair</source>
+        <translation>晴朗</translation>
+    </message>
+    <message>
+        <source>Moderate</source>
+        <translation>温和</translation>
+    </message>
+    <message>
+        <source>Poor</source>
+        <translation>恶劣</translation>
+    </message>
+    <message>
+        <source>Very poor</source>
+        <translation>非常恶劣</translation>
+    </message>
+</context>
+<context>
+    <name>SelectCountryPage</name>
+    <message>
+        <source>Select country</source>
+        <translation>选择国家</translation>
+    </message>
+</context>
+<context>
+    <name>SelectProviderPage</name>
+    <message>
+        <source>Select provider</source>
+        <translation>选择提供商</translation>
+    </message>
+    <message>
+        <source>Manage providers</source>
+        <translation>管理提供商</translation>
+    </message>
+    <message>
+        <source>Find nearest stations</source>
+        <translation>找到最近站点</translation>
+    </message>
+</context>
+<context>
+    <name>SelectProvincePage</name>
+    <message>
+        <source>Show all</source>
+        <translation>显示全部</translation>
+    </message>
+    <message>
+        <source>Find nearest</source>
+        <translation>找到最近</translation>
+    </message>
+</context>
+<context>
+    <name>SensorListModel</name>
+    <message>
+        <source>brak danych</source>
+        <translation type="vanished">brak danych</translation>
+    </message>
+    <message>
+        <source>No data</source>
+        <translation>无数据</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Units type</source>
+        <translation>单位类型</translation>
+    </message>
+    <message>
+        <source>Microgram</source>
+        <translation>微克</translation>
+    </message>
+    <message>
+        <source>Milligram</source>
+        <translation>毫克</translation>
+    </message>
+    <message>
+        <source>GPS</source>
+        <translation>GPS</translation>
+    </message>
+    <message>
+        <source>Never</source>
+        <translation>从不</translation>
+    </message>
+    <message>
+        <source>30 minutes</source>
+        <translation>30 分钟</translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation>1 小时</translation>
+    </message>
+    <message>
+        <source>2 hours</source>
+        <translation>2 小时</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 小时</translation>
+    </message>
+    <message>
+        <source>Update frequency</source>
+        <translation>更新频率</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation><通知/translation>
+    </message>
+    <message>
+        <source>Works only with the screen on. If the screen is off, the program will automatically look for the nearest station after turning the screen on.</source>
+        <translation>仅当屏幕点亮时工作。如果屏幕熄灭，程序将在重新电量屏幕之后自动搜索最近站点。</translation>
+    </message>
+    <message>
+        <source>Manage providers</source>
+        <translation>管理提供商</translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation>已暂停</translation>
+    </message>
+</context>
+<context>
+    <name>StationInfoPage</name>
+    <message>
+        <source>Add to favourite</source>
+        <translation>添加为收藏</translation>
+    </message>
+    <message>
+        <source>Remove from favourite</source>
+        <translation>从收藏移除</translation>
+    </message>
+    <message>
+        <source>Data by </source>
+        <translation>数据</translation>
+    </message>
+</context>
+<context>
+    <name>StationList</name>
+    <message>
+        <source>OTHER</source>
+        <translation type="vanished">OTHER</translation>
+    </message>
+</context>
+<context>
+    <name>StationListModel</name>
+    <message>
+        <source>Nearest station</source>
+        <translation>最近站点</translation>
+    </message>
+    <message>
+        <source>Air pollution in your neighbour is getting worse: </source>
+        <translation>你附近的空气质量状况将会恶化:</translation>
+    </message>
+    <message>
+        <source>Air pollution in your neighbour is getting better: </source>
+        <translation>你附近的空气污染将会改善:</translation>
+    </message>
+    <message>
+        <source>Air pollution is getting worse: </source>
+        <translation>空气污染状况正在恶化:</translation>
+    </message>
+    <message>
+        <source>Air pollution is getting better: </source>
+        <translation>空气污染状况正在改善: </translation>
+    </message>
+</context>
+</TS>
